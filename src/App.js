1afcfde79c9dbaca380e020b65668464d107a705
bong kim <32095134+underbleu@users.noreply.github.com>
2018-01-19 23:06:27 +0900

    get movie API

--- src/App.js
+++ src/App.js
@@ -5,49 +5,39 @@ import Movie from './Movie';
 class App extends Component {
   // Render: componentWillMount() -> render() -> componentDidMount()
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate() 
-  state = {
-  }
+  state = {}
   
-  componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: "Matrics",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
-          },
-          {
-            title: "Lion Kings",
-            poster: "https://images-na.ssl-images-amazon.com/images/I/41kSXzSlnJL.jpg"
-          },
-          {
-            title: "Oldboy",
-            poster: "http://2.bp.blogspot.com/-mFKmi1qP_W0/U2V1vWfq3JI/AAAAAAAAKuk/Ivhy1M2jJdI/s1600/OLDBOY+-+South+Korean+Poster+4.jpg"
-          },
-          {
-            title: "Star Wars",
-            poster: "http://starwarsblog.starwars.com/wp-content/uploads/2017/10/the-last-jedi-theatrical-blog.jpg"
-          }
-        ]
-      })
-    }, 2000)
+  
+  componentDidMount() { // -> after Component mounted, you can get any info from API by using "fetch"
+     this._getMovies(); 
   }
 
-  _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+  _renderMovies = () => { // _ -> naiming system for personal function 
+    const movies = this.state.movies.map(movie => { // Using "index" as parameter for map() method is very very slow! don't use "index" as much as possible
+      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
     })
     return movies
   }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count') // fetch() -> can load API by Ajax(Asyncronous). nice!
+      .then(response => response.json())
+      .then(json => json.data.movies)
+      .catch(err => console.log(err))
+  }
+
 
   render() {
     return (  
       <div className = "App">
         {this.state.movies ? this._renderMovies() : 'Loading'}
-        {/* {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })} */}
       </div>
     );
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,35 +1,62 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component{
-    // 데이터 타입체크 -> 많은 버그 잡을 수 있음
-    static propTypes = {
-        title: PropTypes.string.isRequired,
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster} />
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+// * Stateless Fuctional Component(Dumb)
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-
-    static propTypes = {
-        poster: PropTypes.string.isRequired
-    }
-
-    render(){
-        return(
-            <img src={this.props.poster} />
-        )
-    }
+function MoviePoster({poster}){
+    return (
+        <img src={poster} alt="Movie Poster" />
+    )
 }
+
+// * propTypes Check! 
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired
+}
+
+
+// * State Functional Component (Smart)
+// class Movie extends Component {
+//     // 데이터 타입체크 -> 많은 버그 잡을 수 있음
+//     static propTypes = {
+//         title: PropTypes.string.isRequired,
+//         poster: PropTypes.string.isRequired
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <MoviePoster poster={this.props.poster} />
+//                 <h1>{this.props.title}</h1>
+//             </div>
+//         )
+//     }
+// }
+// class MoviePoster extends Component{
+
+//     static propTypes = {
+//         poster: PropTypes.string.isRequired
+//     }
+
+//     render(){
+//         return(
+//             <img src={this.props.poster} alt={this.props.title}/>
+//         )
+//     }
+// }
 
 export default Movie;
