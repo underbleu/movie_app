ec8b9c43a45d1c2b74eef73ff3ab678c17fd37ee
bong kim <32095134+underbleu@users.noreply.github.com>
2018-01-20 23:13:19 +0900

    complete build.

--- src/App.js
+++ src/App.js
@@ -12,9 +12,16 @@ class App extends Component {
      this._getMovies(); 
   }
 
-  _renderMovies = () => { // _ -> naiming system for personal function 
+  _renderMovies = () => { // _ underbar -> naiming system for personal function 
     const movies = this.state.movies.map(movie => { // Using "index" as parameter for map() method is very very slow! don't use "index" as much as possible
-      return <Movie title={movie.title} poster={movie.medium_cover_image} key={movie.id} />
+      console.log(movie)
+      return <Movie 
+        title={movie.title_english} 
+        poster={movie.medium_cover_image} 
+        key={movie.id} 
+        genres={movie.genres}
+        synopsis={movie.synopsis} 
+      />
     })
     return movies
   }
